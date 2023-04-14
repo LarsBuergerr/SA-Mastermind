ce2329bcd74bdf183e4e70037779370957db425f
SinusP-CW90 <chwerner@htwg-konstanz.de>
2023-04-14 14:14:42 +0200

    Added RootService TODO: import MastermindModule for the Injector

---
+++ Core/src/main/scala/controller/ControllerComponent/RootService.scala
@@ -0,0 +1,63 @@
+/**
+ * RootSevice.scala
+ *  implementation for AKKA RootService
+ */
+
+//****************************************************************************** PACKAGE
+package controller.ControllerComponent
+
+//****************************************************************************** IMPORTS
+import akka.http.scaladsl.server.Directives.{complete, concat, get, path}
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.model.StatusCode
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import akka.http._
+import akka.http.impl.util.JavaAccessors.HttpEntity
+
+import com.google.inject.{Guice, Injector}
+import scala.util.{Failure, Success}
+import Mastermind-Root-Module.MastermindModule //TODO import MastermindModule???
+
+
+object RootService {
+  val injector: Injector = Guice.createInjector(MastermindModule())
+  val controller = injector.getInstance(classOf[ControllerInterface])
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    implicit val executionContext = system.executionContext
+    val servicePort = 8080
+
+    val route =
+      concat (
+        get {
+          path("load") {
+            controller.load
+            complete(HttpEntity(ContentTypes.`application/json`, "loaded"))
+          }
+        },
+        post {
+          path("save") {
+            controller.save
+            complete(HttpEntity(ContentTypes.`application/json`, "saved"))
+          }
+        }
+      )
+
+    val bound = Http().newServerAt("localhost", 8080).bind(route)
+
+    bound.onComplete {
+      case Success(binding) => {
+        print(binding)
+      }
+      case Failure(exception) => {
+
+      }
+    }
+  }
+}
+
