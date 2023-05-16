cd3c6a56ae0fdca456b7e09db27a53cef136655c
lars buerger <lars.buerger@freenet.de>
2023-05-16 17:35:01 +0200

    Changed database structure.

--- Persistence/src/main/scala/database/SlickDB/SlickDAO.scala
+++ Persistence/src/main/scala/database/SlickDB/SlickDAO.scala
@@ -2,7 +2,7 @@ package SlickDB
 
 import FileIOComponent.fileIOJsonImpl.FileIO
 import model.GameComponent.GameInterface
-import SQLTables.{MatrixTable, TurnTable, CodeTable, GameTable, GameTable2, StateTable}
+import SQLTables.{MatrixTable, TurnTable, CodeTable, GameTable, GameTable2, StateTable, HMatrixTable}
 import slick.jdbc.JdbcBackend.Database
 
 import java.sql.SQLNonTransientException
@@ -40,16 +40,20 @@ class SlickDAO extends DAOInterface {
     password = databasePassword
   )
   val matrixTable = new TableQuery(new MatrixTable(_))
+  val hmatrixTable = new TableQuery(new HMatrixTable(_))
   val turnTable = new TableQuery(new TurnTable(_))
   val codeTable = new TableQuery(new CodeTable(_))
   val gameTable = new TableQuery(new GameTable(_))
   val stateTable = new TableQuery(new StateTable(_))
   val gameTable2 = new TableQuery(new GameTable2(_))
 
-  val setup: DBIOAction[Unit, NoStream, Effect.Schema] = DBIO.seq(matrixTable.schema.createIfNotExists, 
-                                                                  turnTable.schema.createIfNotExists, 
+  val setup: DBIOAction[Unit, NoStream, Effect.Schema] = DBIO.seq(matrixTable.schema.createIfNotExists,
+                                                                  hmatrixTable.schema.createIfNotExists,
+                                                                  turnTable.schema.createIfNotExists,
                                                                   codeTable.schema.createIfNotExists,
-                                                                  gameTable.schema.createIfNotExists)
+                                                                  gameTable.schema.createIfNotExists,
+                                                                  gameTable2.schema.createIfNotExists,
+                                                                  stateTable.schema.createIfNotExists)
   println("create tables")
   try {
     Await.result(database.run(setup), WAIT_TIME)
@@ -61,19 +65,88 @@ class SlickDAO extends DAOInterface {
   }
   println("tables created")
 
-  override def load(id: Option[Int] = None): Try[GameInterface] = {
+  // override def load(id: Option[Int] = None): Try[GameInterface] = {
+  //   Try {
+  //     print("before query")
+  //     val query = id.map(id => gameTable.filter(_.id === id))
+  //       .getOrElse(gameTable.filter(_.id === gameTable.map(_.id).max))
+
+  //     val game = Await.result(database.run(query.result), WAIT_TIME)
+
+  //     val matrix = game.head._2
+  //     val hmatrix = game.head._3
+  //     val code = game.head._4
+  //     val turn = game.head._5
+  //     val state = sanitize(game.head._6)
+
+  //     val jsonGame = Json.obj(
+  //       "matrix" -> Json.parse(matrix),
+  //       "hmatrix" -> Json.parse(hmatrix),
+  //       "code" -> Json.parse(code),
+  //       "turn" -> Json.toJson(turn),
+  //       "state" -> Json.parse(state)
+  //     )
+  //     val res = fileIO.jsonToGame(jsonGame.asInstanceOf[JsValue])
+  //     res
+  //   }
+  // }
+
+  // override def save(game: GameInterface) =
+  //   Try {
+  //     print("Saving game into database...\n")
+  //     val jsonGame = fileIO.gameToJson(game)
+      
+  //     val gameID = storeGame(
+  //       jsonGame("matrix").toString(),
+  //       jsonGame("hmatrix").toString(),
+  //       jsonGame("code").toString(),
+  //       jsonGame("turn").toString().toInt,
+  //       jsonGame("state").toString()
+  //     )
+  //   }
+
+  override def save(game: GameInterface) =
     Try {
-      print("before query")
-      val query = id.map(id => gameTable.filter(_.id === id))
-        .getOrElse(gameTable.filter(_.id === gameTable.map(_.id).max))
+      val json_game = fileIO.gameToJson(game)
+
+      val matrixID = storeMatrix(json_game("matrix").toString())
+
+      val hmatrixID = storeHMatrix(json_game("hmatrix").toString())
+
+      val codeID = storeCode(json_game("code").toString())
+
+      val turnID = storeTurn(json_game("turn").toString().toInt)
+
+      val stateID = storeState(json_game("state").toString())
+
+      val gameID = storeGame(
+        matrixID,
+        hmatrixID,
+        codeID,
+        turnID,
+        stateID
+      )
+    }
+
+
+  override def load(id: Option[Int] = None) =
+    Try {
+      val query = id.map(id => gameTable2.filter(_.id === id))
+        .getOrElse(gameTable2.filter(_.id === gameTable2.map(_.id).max))
 
       val game = Await.result(database.run(query.result), WAIT_TIME)
 
-      val matrix = game.head._2
-      val hmatrix = game.head._3
-      val code = game.head._4
-      val turn = game.head._5
-      val state = sanitize(game.head._6)
+      val matrixID = game.head._2
+      val hmatrixID = game.head._3
+      val codeID = game.head._4
+      val turnID = game.head._5
+      val stateID = game.head._6
+
+      val matrix = Await.result(database.run(matrixTable.filter(_.id === matrixID).result), WAIT_TIME).head._2
+      val hmatrix = Await.result(database.run(hmatrixTable.filter(_.id === hmatrixID).result), WAIT_TIME).head._2
+      val code = Await.result(database.run(codeTable.filter(_.id === codeID).result), WAIT_TIME).head._2
+      val turn = Await.result(database.run(turnTable.filter(_.id === turnID).result), WAIT_TIME).head._2
+      val state = Await.result(database.run(stateTable.filter(_.id === stateID).result), WAIT_TIME).head._2
 
       val jsonGame = Json.obj(
         "matrix" -> Json.parse(matrix),
@@ -85,76 +158,7 @@ class SlickDAO extends DAOInterface {
       val res = fileIO.jsonToGame(jsonGame.asInstanceOf[JsValue])
       res
     }
-  }
 
-  override def save(game: GameInterface) =
-    Try {
-      print("Saving game into database...\n")
-      val jsonGame = fileIO.gameToJson(game)
-      
-      val gameID = storeGame(
-        jsonGame("matrix").toString(),
-        jsonGame("hmatrix").toString(),
-        jsonGame("code").toString(),
-        jsonGame("turn").toString().toInt,
-        jsonGame("state").toString()
-      )
-    }
-
-  def save2(game: GameInterface) =
-    val json_game = fileIO.gameToJson(game)
-
-    val matrixID = storeMatrix(json_game("matrix").toString())
-
-    val hmatrixID = storeHMatrix(json_game("hmatrix").toString())
-
-    val codeID = storeCode(json_game("code").toString())
-
-    val turnID = storeTurn(json_game("turn").toString().toInt)
-
-    val stateID = storeState(json_game("state").toString())
-
-    val gameID = storeGame2(
-      matrixID,
-      hmatrixID,
-      codeID,
-      turnID,
-      stateID
-    )
-
-  def load2(id: Option[Int] = None) =
-    val query = id.map(id => gameTable2.filter(_.id === id))
-      .getOrElse(gameTable2.filter(_.id === gameTable2.map(_.id).max))
-
-    val game = Await.result(database.run(query.result), WAIT_TIME)
-
-    val matrixID = game.head._2
-    val hmatrixID = game.head._3
-    val codeID = game.head._4
-    val turnID = game.head._5
-    val stateID = game.head._6
-
-    val matrix = Await.result(database.run(matrixTable.filter(_.id === matrixID).result), WAIT_TIME).head._2
-    val hmatrix = Await.result(database.run(matrixTable.filter(_.id === hmatrixID).result), WAIT_TIME).head._2
-    val code = Await.result(database.run(codeTable.filter(_.id === codeID).result), WAIT_TIME).head._2
-    val turn = Await.result(database.run(turnTable.filter(_.id === turnID).result), WAIT_TIME).head._2
-    val state = Await.result(database.run(stateTable.filter(_.id === stateID).result), WAIT_TIME).head._2
-
-    val jsonGame = Json.obj(
-      "matrix" -> Json.parse(matrix),
-      "hmatrix" -> Json.parse(hmatrix),
-      "code" -> Json.parse(code),
-      "turn" -> Json.toJson(turn),
-      "state" -> Json.parse(state)
-    )
-    val res = fileIO.jsonToGame(jsonGame.asInstanceOf[JsValue])
-    res
-
-  def storeGame2(matrixID: Int, hmatrixID: Int, codeID: Int, turnID: Int, stateID: Int) = {
-    val gameID = Await.result(database.run(gameTable2 returning gameTable2.map(_.id) += (0, matrixID, hmatrixID, codeID, turnID, stateID)), WAIT_TIME)
-    gameID
-  }
-  
   override def delete(id: Int): Try[Boolean] = {
     Try {
       Await.result(database.run(gameTable.filter(_.id === id).delete), WAIT_TIME)
@@ -173,7 +177,11 @@ class SlickDAO extends DAOInterface {
       true
     }
   }
-
+  def storeGame(matrixID: Int, hmatrixID: Int, codeID: Int, turnID: Int, stateID: Int) = {
+    val gameID = Await.result(database.run(gameTable2 returning gameTable2.map(_.id) += (0, matrixID, hmatrixID, codeID, turnID, stateID)), WAIT_TIME)
+    gameID
+  }
+  
   def listAllGames() = {
     val query = gameTable
     val games = Await.result(database.run(query.result), WAIT_TIME)
@@ -181,14 +189,9 @@ class SlickDAO extends DAOInterface {
   }
 
   def printGames(games: Seq[(Int, String, String, String, Int, String)]) = {
-
+    println("All saved Games: ")
+    println("-----------------")
     games.foreach(game => println("GameID: " + game._1))
-  }
-
-
-  def storeGame(matrix: String, hmatrix: String, code: String, turn: Int, state: String) = {
-    val gameID = Await.result(database.run(gameTable returning gameTable.map(_.id) += (0, matrix, hmatrix, code, turn, state)), WAIT_TIME)
-    gameID
   }
 
   def storeTurn(turn: Int) = {
@@ -207,12 +210,12 @@ class SlickDAO extends DAOInterface {
   }
 
   def storeHMatrix(hmatrix: String) = {
-    val hmatrixID = Await.result(database.run(matrixTable returning matrixTable.map(_.id) += (0, hmatrix)), WAIT_TIME)
+    val hmatrixID = Await.result(database.run(hmatrixTable returning hmatrixTable.map(_.id) += (0, hmatrix)), WAIT_TIME)
     hmatrixID
   }
 
   def storeState(state: String) = {
-    val stateID = Await.result(database.run(matrixTable returning matrixTable.map(_.id) += (0, state)), WAIT_TIME)
+    val stateID = Await.result(database.run(stateTable returning stateTable.map(_.id) += (0, state)), WAIT_TIME)
     stateID
   }
 
